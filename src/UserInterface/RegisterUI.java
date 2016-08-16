188c35d3868d2b95fcbe4b7d7c5b559e88f9ec57
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-08-15 21:15:18 -0300

    5th cp

--- src/UserInterface/RegisterUI.java
+++ src/UserInterface/RegisterUI.java
@@ -1,8 +1,17 @@
 
 package UserInterface;
 
-public class RegisterUI extends {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class RegisterUI extends JFrame{
     
-    
+    /*tfRnumber to campo de texto para matricula, jlRNumber to label matricula*/
+       
+    private JTextField tfName, tfRnumber, tfLanguage, tfPhone;
+    private JButton jbInserir, jbClean;
+    private JLabel jlName, jlRnumber,jlPhone, jlLanguage, jlMon, jlTue, jlWed, jlThu, jlFri, jlSat; 
     
 }
