7dd6beabeacf0913a9729918892b20eb9ee3fe26
Dicksonrsg <dickson.el@gmail.com>
2016-08-16 10:21:27 -0300

    register changes

--- src/UserInterface/RegisterUI.java
+++ src/UserInterface/RegisterUI.java
@@ -13,5 +13,50 @@ public class RegisterUI extends JFrame{
     private JTextField tfName, tfRnumber, tfLanguage, tfPhone;
     private JButton jbInserir, jbClean;
     private JLabel jlName, jlRnumber,jlPhone, jlLanguage, jlMon, jlTue, jlWed, jlThu, jlFri, jlSat; 
+
+    public RegisterUI(){
+        startComponents();
+        setEvents();    	
+    }
     
+	private void startComponents() {
+		setLayout(null);
+		
+		tfName = new JTextField();
+		tfRnumber = new JTextField();
+		tfLanguage = new JTextField();
+		tfPhone = new JTextField();
+		
+		jbInserir = new JButton("Cadastrar");
+		jbClean = new JButton("Limpar");
+		
+		jlName = new JLabel("Nome");
+		jlRnumber = new JLabel("Matricula");
+		jlPhone = new JLabel("Telefone");
+		jlLanguage = new JLabel("Lingua");
+		jlMon = new JLabel("Segunda");
+		jlTue = new JLabel("Terça");
+		jlWed = new JLabel("Quarta");
+		jlThu = new JLabel("Quinta");
+		jlFri = new JLabel("Sexta");
+		jlSat = new JLabel("Sabado");
+		
+		jlRnumber.setBounds(10, 15, 40, 20);
+		jlName.setBounds(10, 40, 45, 20);
+		jlLanguage.setBounds(10, 65, 45, 20);
+		jlPhone.setBounds(10, 90, 50, 20);
+		
+		add(jlRnumber); add(jlName); add(jlLanguage); add(jlPhone);
+		
+/*		add(); add();
+		
+		add(); add(); add(); add(); add(); add(); add(); add(); add(); add();*/
+		
+	}
+	
+	private void setEvents() {
+		
+		
+	}
+        
 }
