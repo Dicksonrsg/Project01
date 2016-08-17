93fe6a96c1f9da1adc42ffc4db151f4ee8be4084
a923181 <a923181@02LAB108PC03.rn.senac.br>
2016-08-16 21:19:37 -0300

    8th

--- src/UserInterface/RegisterUI.java
+++ src/UserInterface/RegisterUI.java
@@ -1,6 +1,10 @@
 
 package UserInterface;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -50,13 +54,43 @@ public class RegisterUI extends JPanel{
 		rbFri = new JRadioButton("Sexta");
 		rbSat = new JRadioButton("Sabado");
                 
-                jcMS1 = new JCheckBox("07:20 - 09:20"); 
-                jcMS2 = new JCheckBox("09:30 - 11:30");
-                jcMS3 = new JCheckBox("13:30 - 15:30");
-                jcMS4 = new JCheckBox("15:40 - 17:40");
-                jcMS5 = new JCheckBox("18:00 - 20:00");
-                jcMS6 = new JCheckBox("20:00 - 22:00");
+                jcMS1 = new JCheckBox("07:20 - 09:20"); jcMS1.setEnabled(false);
+                jcMS2 = new JCheckBox("09:30 - 11:30"); jcMS2.setEnabled(false);
+                jcMS3 = new JCheckBox("13:30 - 15:30"); jcMS3.setEnabled(false);
+                jcMS4 = new JCheckBox("15:40 - 17:40"); jcMS4.setEnabled(false);
+                jcMS5 = new JCheckBox("18:00 - 20:00"); jcMS5.setEnabled(false);
+                jcMS6 = new JCheckBox("20:00 - 22:00"); jcMS6.setEnabled(false);
+                
+                jcTS1 = new JCheckBox("07:20 - 09:20"); jcTS1.setEnabled(false);
+                jcTS2 = new JCheckBox("09:30 - 11:30"); jcTS2.setEnabled(false);
+                jcTS3 = new JCheckBox("13:30 - 15:30"); jcTS3.setEnabled(false);
+                jcTS4 = new JCheckBox("15:40 - 17:40"); jcTS4.setEnabled(false);
+                jcTS5 = new JCheckBox("18:00 - 20:00"); jcTS5.setEnabled(false);
+                jcTS6 = new JCheckBox("20:00 - 22:00"); jcTS6.setEnabled(false);
+                
+                jcWS1 = new JCheckBox("07:20 - 09:20"); jcWS1.setEnabled(false);
+                jcWS2 = new JCheckBox("09:30 - 11:30"); jcWS2.setEnabled(false);
+                jcWS3 = new JCheckBox("13:30 - 15:30"); jcWS3.setEnabled(false);
+                jcWS4 = new JCheckBox("15:40 - 17:40"); jcWS4.setEnabled(false);
+                jcWS5 = new JCheckBox("18:00 - 20:00"); jcWS5.setEnabled(false);
+                jcWS6 = new JCheckBox("20:00 - 22:00"); jcWS6.setEnabled(false);
+                
+                jcThS1 = new JCheckBox("07:20 - 09:20"); jcThS1.setEnabled(false);
+                jcThS2 = new JCheckBox("09:30 - 11:30"); jcThS2.setEnabled(false);
+                jcThS3 = new JCheckBox("13:30 - 15:30"); jcThS3.setEnabled(false);
+                jcThS4 = new JCheckBox("15:40 - 17:40"); jcThS4.setEnabled(false);
+                jcThS5 = new JCheckBox("18:00 - 20:00"); jcThS5.setEnabled(false);
+                jcThS6 = new JCheckBox("20:00 - 22:00"); jcThS6.setEnabled(false);              
 		
+                jcFS1 = new JCheckBox("07:20 - 09:20"); jcFS1.setEnabled(false);
+                jcFS2 = new JCheckBox("09:30 - 11:30"); jcFS2.setEnabled(false);
+                jcFS3 = new JCheckBox("13:30 - 15:30"); jcFS3.setEnabled(false);
+                jcFS4 = new JCheckBox("15:40 - 17:40"); jcFS4.setEnabled(false);
+                jcFS5 = new JCheckBox("18:00 - 20:00"); jcFS5.setEnabled(false);
+                jcFS6 = new JCheckBox("20:00 - 22:00"); jcFS6.setEnabled(false);
+                
+                jcSatS = new JCheckBox("08:00 - 12:00"); jcSatS.setEnabled(false);
+                
 		jlRnumber.setBounds(10, 15, 60, 20);
 		jlName.setBounds(10, 40, 45, 20);
 		jlLanguage.setBounds(10, 65, 45, 20);
@@ -81,6 +115,36 @@ public class RegisterUI extends JPanel{
                 jcMS4.setBounds(10, 220, 100, 20);
                 jcMS5.setBounds(10, 245, 100, 20);
                 jcMS6.setBounds(10, 270, 100, 20);
+                
+                jcTS1.setBounds(120, 145, 100, 20);
+                jcTS2.setBounds(120, 170, 100, 20);
+                jcTS3.setBounds(120, 195, 100, 20);
+                jcTS4.setBounds(120, 220, 100, 20);
+                jcTS5.setBounds(120, 245, 100, 20);
+                jcTS6.setBounds(120, 270, 100, 20);  
+                
+                jcWS1.setBounds(230, 145, 100, 20);
+                jcWS2.setBounds(230, 170, 100, 20);
+                jcWS3.setBounds(230, 195, 100, 20);
+                jcWS4.setBounds(230, 220, 100, 20);
+                jcWS5.setBounds(230, 245, 100, 20);
+                jcWS6.setBounds(230, 270, 100, 20);
+                
+                jcThS1.setBounds(340, 145, 100, 20);
+                jcThS2.setBounds(340, 170, 100, 20);
+                jcThS3.setBounds(340, 195, 100, 20);
+                jcThS4.setBounds(340, 220, 100, 20);
+                jcThS5.setBounds(340, 245, 100, 20);
+                jcThS6.setBounds(340, 270, 100, 20);
+                
+                jcFS1.setBounds(450, 145, 100, 20);
+                jcFS2.setBounds(450, 170, 100, 20);
+                jcFS3.setBounds(450, 195, 100, 20);
+                jcFS4.setBounds(450, 220, 100, 20);
+                jcFS5.setBounds(450, 245, 100, 20);
+                jcFS6.setBounds(450, 270, 100, 20);
+                
+                jcSatS.setBounds(560, 145, 100, 20);
 		
 		add(jlRnumber); add(jlName); add(jlLanguage); add(jlPhone);
 		
@@ -89,12 +153,91 @@ public class RegisterUI extends JPanel{
 		add(rbMon); add(rbTue); add(rbWed); add(rbThu); add(rbFri); add(rbSat);
                 
                 add(jcMS1); add(jcMS2); add(jcMS3); add(jcMS4); add(jcMS5); add(jcMS6);
+                
+                add(jcTS1); add(jcTS2); add(jcTS3); add(jcTS4); add(jcTS5); add(jcTS6);
+                
+                add(jcWS1); add(jcWS2); add(jcWS3); add(jcWS4); add(jcWS5); add(jcWS6);
+                
+                add(jcThS1); add(jcThS2); add(jcThS3); add(jcThS4); add(jcThS5); add(jcThS6);
 		
+                add(jcFS1); add(jcFS2); add(jcFS3); add(jcFS4); add(jcFS5); add(jcFS6);
+                
+                add(jcSatS);
 	}
 	
 	private void setEvents() {
-		
-		
+	rbMon.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcMS1.setEnabled(true);
+                jcMS2.setEnabled(true);
+                jcMS3.setEnabled(true);
+                jcMS4.setEnabled(true);
+                jcMS5.setEnabled(true);
+                jcMS6.setEnabled(true);
+            }
+        });
+        
+	rbTue.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcTS1.setEnabled(true);
+                jcTS2.setEnabled(true);
+                jcTS3.setEnabled(true);
+                jcTS4.setEnabled(true);
+                jcTS5.setEnabled(true);
+                jcTS6.setEnabled(true);
+            }
+        });
+        
+	rbWed.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcWS1.setEnabled(true);
+                jcWS2.setEnabled(true);
+                jcWS3.setEnabled(true);
+                jcWS4.setEnabled(true);
+                jcWS5.setEnabled(true);
+                jcWS6.setEnabled(true);
+            }
+        }); 
+        
+	rbThu.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcThS1.setEnabled(true);
+                jcThS2.setEnabled(true);
+                jcThS3.setEnabled(true);
+                jcThS4.setEnabled(true);
+                jcThS5.setEnabled(true);
+                jcThS6.setEnabled(true);
+            }
+        });        
+	
+	rbFri.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcFS1.setEnabled(true);
+                jcFS2.setEnabled(true);
+                jcFS3.setEnabled(true);
+                jcFS4.setEnabled(true);
+                jcFS5.setEnabled(true);
+                jcFS6.setEnabled(true);
+            }
+        });
+        
+	rbSat.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jcSatS.setEnabled(true);
+            }
+        });        
 	}
         
 }
